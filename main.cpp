4a1a34ac5d9dacc09ec90e47cd14c359a29aaab0
UroBit <139574903+Urobitz@users.noreply.github.com>
2025-08-12 11:39:39 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sqlite3.h>
+
+void printTitle()
+{
+    std::cout << "*********************\n";
+    std::cout << "* Contact Book App *\n";
+    std::cout << "*********************\n"; 
+}
+
+
+int main()
+{
+    sqlite3 * database_handle = nullptr;
+    
+    signed int error_code = 0;
+
+    error_code = sqlite3_open("Contact_Book.db", &database_handle);
+
+    if (error_code != SQLITE_OK)
+    {
+        std::cerr << "Failed to open database." << std::endl;
+        return 1;
+    }
+
+    printTitle();
+
+}
---
+++ sql_functions.h
@@ -0,0 +1,8 @@
+#ifndef SQL_FUNCTIONS_H
+#define SQL_FUNCTIONS_H
+
+#include "sql_functions.cpp"
+
+int createTable(sqlite3 *db);
+
+#endif 
